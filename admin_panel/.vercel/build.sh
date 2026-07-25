d0a4b67a4c65e781438bf8f97f09af0c91e5f4f8
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-07-25 18:47:59 +0600

    Move Vercel Flutter build into script

---
+++ admin_panel/.vercel/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ -d "flutter/.git" ]; then
+  git -C flutter fetch --depth 1 origin stable
+  git -C flutter checkout stable
+else
+  git clone --depth 1 --branch stable https://github.com/flutter/flutter.git
+fi
+
+flutter/bin/flutter config --enable-web
+flutter/bin/flutter pub get
+flutter/bin/flutter build web --release --no-pub
